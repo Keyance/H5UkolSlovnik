772df9fa219cbcfb0cd308b80762109f6205501a
Keyance <klara.konopacova@seznam.cz>
2023-11-14 12:37:13 +0100

    Přidejte soubory projektu.

---
+++ H5UkolSlovnik/Program.cs
@@ -0,0 +1,103 @@
+﻿var slovnik = new Dictionary<string, string>();
+
+string cesta = @"e:\\text\\czechitas\\slovnik.txt";
+
+if (File.Exists(cesta))
+{
+    Console.WriteLine("Soubor existuje.");
+}
+else
+{
+    Console.WriteLine("Soubor neexistuje.");
+    File.Create(cesta);
+}
+
+string[] radky = File.ReadAllLines(cesta);
+string celyText;
+
+if (radky.Length > 0)
+{
+    celyText = File.ReadAllText(cesta);
+
+    string[] pary = celyText.Split(';');
+
+    foreach (string par in pary)
+    {
+        string[]jednotlive = par.Split("=");
+        slovnik.Add(jednotlive[0], jednotlive[1]); //index out of range???
+    }
+}
+
+else
+{
+    Console.WriteLine("Soubor je zatím prázdný.");
+}
+
+
+bool jeKonec = false;
+
+while (!jeKonec)
+{
+    Console.WriteLine("1 - Přidat položku do slovníku");
+    Console.WriteLine("2 - Vypsat celý slovník");
+    Console.WriteLine("3 - Vyhledej dle českého slova");
+    Console.WriteLine("4 - Vyhledej dle anglického slova");
+    Console.WriteLine("0 - Ukončit a nahrát do souboru");
+
+    int volba = Convert.ToInt32(Console.ReadLine());
+
+
+    switch (volba)
+    {
+        case 0:
+            File.WriteAllText(cesta, "");
+            foreach (var polozka in slovnik)
+            {
+                string jedenZaznam = polozka.Value + "="+ polozka.Key + ";";
+                File.AppendAllText(cesta, jedenZaznam);
+            }
+            jeKonec = true;
+            break;
+        case 1:
+            {
+                Console.WriteLine("Zadej český výraz:");
+                string cz = Console.ReadLine().ToLower();
+                if (!slovnik.ContainsKey(cz))
+                {
+                    Console.WriteLine("Zadej anglické synonymum:");
+                    string ang = Console.ReadLine().ToLower();
+                    slovnik.Add(cz, ang);
+                }
+                else { Console.WriteLine("Tento výraz již v seznamu existuje."); }
+                break;
+            }
+        case 2:
+           foreach (KeyValuePair<string, string> kontakt in slovnik)
+            {
+                Console.WriteLine($"{kontakt.Key}\t{kontakt.Value}");
+            }
+            break;
+        case 3:
+            {
+                Console.WriteLine("Zadej počátek hledaného českého výrazu:");
+                string pocatek = Console.ReadLine();
+                var jmeno = slovnik.Where(x => x.Key.StartsWith(pocatek)).Select(z => z.Value).ToList();
+                foreach (var jm in jmeno)
+                {
+                    Console.WriteLine(jm);
+                }
+            }
+            break;
+        case 4:
+            {
+                Console.WriteLine("Zadej počátek hledaného hesla v angličtině:");
+                string pocatek = Console.ReadLine();
+                var jmeno = slovnik.Where(x => x.Value.StartsWith(pocatek)).Select(z => z.Key).ToList();
+                foreach (var jm in jmeno)
+                {
+                    Console.WriteLine(jm);
+                }
+            }
+            break;
+    }
+}
